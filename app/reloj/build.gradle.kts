35eafc7ade08f6a123ee9dc0fa55906d27c2fcc2
bojeda <brandon05251997@gmail.com>
2025-11-20 13:58:27 -0700

    Reloj se comunica con el telefono

--- app/reloj/build.gradle.kts
+++ app/reloj/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.reloj"
+        applicationId = "com.example.examplemvvm"
         minSdk = 30
         targetSdk = 36
         versionCode = 1
